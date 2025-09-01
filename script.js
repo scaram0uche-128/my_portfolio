edee2b6b9cd4fc4b76d26710a3c66d2248d72af0
Prashant Kumar <academic.prashk@gmail.com>
2025-09-01 12:28:41 +0530

    updated script.js

--- script.js
+++ script.js
@@ -16,26 +16,23 @@ closeBtn.addEventListener("click", toggleMenu);
 
 
 // For the Projects page:
-
-// Select buttons and cards
 const filterButtons = document.querySelectorAll('.filter-buttons button');
 const projectCards = document.querySelectorAll('.project-card');
 
 filterButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // Remove 'active' from all buttons
     filterButtons.forEach(btn => btn.classList.remove('active'));
-    // Add 'active' to clicked button
     button.classList.add('active');
 
     const filterValue = button.getAttribute('data-filter');
 
     projectCards.forEach(card => {
       if (filterValue === "all" || card.getAttribute('data-category') === filterValue) {
-        card.style.display = "block";  // Show
+        card.style.display = "block"; 
       } else {
-        card.style.display = "none";   // Hide
+        card.style.display = "none"; 
       }
     });
   });
+
 });
